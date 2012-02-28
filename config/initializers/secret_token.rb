68d9299d2930558352e4645d29f9ccd84fe26370
Zach A <zba@andrew.cmu.edu>
2012-02-27 23:31:00 -0500

    Initial commit of rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phase2::Application.config.secret_token = '1d16e332a93facd00b0a2dfa0f459ab60541c28c343acb47a07cf61af6dcddb0f477b6889ceb046b2f44de0ea172dc707230d455a536e4cd998e141df3291e69'
